ed4653da994bd6d4ad927408f4b90e2dd948496a
sabinabakirzada <bd6zz1ghz@code.edu.az>
2023-12-21 00:42:26 +0400

    demo file

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Bodyo from "./Bodyo";
+import BodyTwo from "./BodyTwo";
+import Portfolio from "./Portfolio";
+import SmallParallax from "./SmallParallax";
+
+const Body = () => {
+  return (
+    <div>
+      <Bodyo />
+      <BodyTwo />
+      <Portfolio />
+      <SmallParallax />
+    </div>
+  );
+};
+
+export default Body;
